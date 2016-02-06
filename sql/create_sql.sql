9a57375cff454e2e0f493f03732aec1fa78ca431
bit-user <bit-user@bit>
2016-02-06 15:21:46 +0900

    2월 6일 쿼리수정

--- sql/create_sql.sql
+++ sql/create_sql.sql
@@ -41,19 +41,7 @@ CREATE SEQUENCE USER_SEQ START WITH 0 INCREMENT BY 1 NOMAXVALUE MINVALUE 0;
 CREATE SEQUENCE BOARD_SEQ START WITH 0 INCREMENT BY 1 NOMAXVALUE MINVALUE 0;
 
 INSERT INTO TB_USER VALUES(USER_SEQ.NEXTVAL,'Admin','Admin','Admin@admin.com','010-1234-1234',99,sysdate);
-insert into TB_USER values(USER_SEQ.nextval,'user2','user2','user2@gmail.com','010-1234-5678',32,sysdate);
-insert into TB_USER values(USER_SEQ.nextval,'user3','user3','user3@gmail.com','010-1111-1111',33,sysdate);
-insert into TB_USER values(USER_SEQ.nextval,'user4','user4','user4@gmail.com','010-2222-2222',34,sysdate);
-insert into TB_USER values(USER_SEQ.nextval,'user5','user5','user5@gmail.com','010-3333-3333',35,sysdate);
-insert into TB_USER values(USER_SEQ.nextval,'user6','user6','user6@gmail.com','010-4444-4444',36,sysdate);
-insert into TB_USER values(USER_SEQ.nextval,'user7','user7','user7@gmail.com','010-5555-5555',37,sysdate);
 commit;
+
 insert into tb_board values(BOARD_SEQ.nextval,'user1','user1',' 1',sysdate);
-insert into tb_board values(BOARD_SEQ.nextval,'user2','user22','2',sysdate);
-insert into tb_board values(BOARD_SEQ.nextval,'user3','user333',' 3',sysdate);
-insert into tb_board values(BOARD_SEQ.nextval,'user4','user4444','4',sysdate);
-insert into tb_board values(BOARD_SEQ.nextval,'user5','user55555','5',sysdate);
-insert into tb_board values(BOARD_SEQ.nextval,'user6','user666666',' 6',sysdate);
-insert into tb_board values(BOARD_SEQ.nextval,'user7','user7777777',' 7',sysdate);
-insert into tb_board values(BOARD_SEQ.nextval,'user8','user88888888',' 8',sysdate);
 commit;
